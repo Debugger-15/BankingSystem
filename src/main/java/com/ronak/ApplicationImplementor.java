b644967563358fe93fb6ea9a9bc2b919cdae66e5
Ronak_15 <Ronak_15@192.168.0.6>
2019-01-05 00:06:12 +0530

    added implementor

---
+++ src/main/java/com/ronak/ApplicationImplementor.java
@@ -0,0 +1,12 @@
+package com.ronak;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class ApplicationImplementor implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+}
